5034d33ea5ccd29c1119f7ed57a9d91b39b752f1
jjokah <johnjohnsonokah@gmail.com>
2023-12-13 15:26:07 +0100

    Run and deploy celebration-classifier model

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+# %%
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+# %%
+learn = load_learner('model.pkl')
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+# %%
+im = PILImage.create('christmas.jpeg')
+im.thumbnail((192, 192))
+im
+
+# %%
+learn.predict(im)
+
+# %%
+categories = ('christmas', 'easter', 'ramadan')
+
+def classify_image(img):
+    pred, idx, probs = learn.predict(img)
+    return dict(zip(categories, map(float, probs)))
+
+# %%
+classify_image(im)
+
+# %%
+examples = ['christmas.jpeg', 'easter.jpeg', 'ramadan.jpeg']
+
+intf = gr.Interface(fn=classify_image, inputs="image", outputs="label", examples=examples)
+intf.launch(inline=False)
